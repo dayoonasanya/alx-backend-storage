414c8b399f47edc052e0a6b3f5f8cdbc14fc90d3
Adedayo Onasanya <aonasanya@mail.dccc.edu>
2024-07-16 00:14:21 +0300

    Update 12-log_stats.py

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,11 +1,5 @@
 #!/usr/bin/env python3
-"""
-Provide some stats about Nginx logs stored in MongoDB
-Database: logs, Collection: nginx, Display same as example
-first line: x logs, x number of documents in this collection
-second line: Methods
-5 lines with method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-one line with method=GET, path=/status
+"""log stats from collection
 """
 from pymongo import MongoClient
 
@@ -14,9 +8,7 @@ METHODS = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
 
 def log_stats(mongo_collection, option=None):
-    """
-    Prototype: def log_stats(mongo_collection, option=None):
-    Provide some stats about Nginx logs stored in MongoDB
+    """ script that provides some stats about Nginx logs stored in MongoDB
     """
     items = {}
     if option:
